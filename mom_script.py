9536cebbfb7c09149319e7435e9093c911f5a20b
Daniel Fehder <dfehder@gmail.com>
2011-08-25 18:04:10 -0400

    beginning to add error handling for other files

--- mom_script.py
+++ mom_script.py
@@ -7,6 +7,33 @@ import re, os, csv, sys, mom_parser
 if "C:\\emacs\\mom_parser\\" not in sys.path:
     sys.path.append("C:\\emacs\\mom_parser")
 
+#This is the function to check the existice of an entrie in the remapping table in the a_nutrient_observation
+def find_key(dic, val):
+    """return the key of dictionary dic given the value"""
+    try:
+        return [k for k, v in dic.iteritems() if v == val]
+    
+    except IndexError:
+        pass
+
+#I am going to take from below and put it into the key_checker
+o = <strong class="highlight">open</strong>("output","a") #open for append
+for line in <strong class="highlight">open</strong>("<strong class="highlight">file</strong>"):
+       line = line.replace("someword","newword")
+   o.write(line + "\n") 
+o.close()
+
+def key_checker(goodKeyList, mappingKeys,  file):
+    fl = open(file, 'r+')
+    read = fl.read()
+
+    for key in goodKeyList:
+        #this checks to see if the key exists
+        if re.search(re.escape(key),read):
+            
+        
+    
+
 
 ## initializations . . . eventually we want to loope over all the open files 
 file_in_path = "C:\\emacs\\mom_parser"
@@ -17,7 +44,7 @@ file_list = os.listdir(file_in_path)
 for files in file_list:
     if files[-3:] == 'txt':
 
-        f = mom_parser.nutrient_observation()
+        f = mom_parser.a_nutrient_observation()
         f.parse_data(file_in_path, files)
         f.csv_writer(file_in_path)
 
